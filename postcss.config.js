cee730baa2a47f9f6c10372998875c5d44caaa2c
Steffie Hermans <steffie.hermans@student.howest.be>
2018-01-18 19:05:37 +0100

    image dependencies toegevoegd als ook url-loader

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,8 @@ module.exports = {
   plugins: [
     require(`stylelint`),
     require(`postcss-reporter`)({clearReportedMessages: true}),
+    require(`postcss-will-change`),
+    require(`postcss-color-function`),
     require(`postcss-import`),
     require(`postcss-cssnext`),
   ]
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require(`path`);
 const webpack = require(`webpack`);
 
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const imageminJpegRecompress = require("imagemin-jpeg-recompress");
+
 const PATHS = {
   src: path.join(__dirname, `src`),
   dist: path.join(__dirname, `dist`),
@@ -39,16 +42,45 @@ const commonConfig = merge([ {
           }
         }
         ]
-      },{
+      },
+      {
         test: /\.html$/,
         loader: `html-loader`
-      },{
-        test: /\.jpe?g$/,
-        loader: `url-loader`,
-        options:{
-          limit:1000,
-          context:`./src`,
-          name:`[path][name].[ext]`,
+      },
+      {
+          test: /\.(jpe?g|png|gif|webp|svg)$/,
+          use: [
+            {
+              loader: `file-loader`,
+              options: {
+                limit: 1000,
+                context: `./src`,
+                name: `[path][name].[ext]`
+              }
+            },
+            {
+              loader: `image-webpack-loader`,
+              options: {
+                bypassOnDebug: true,
+                mozjpeg: {
+                  progressive: true,
+                  quality: 65
+                },
+                optipng: {
+                  enabled: false
+                },
+                pngquant: {
+                  quality: "65-90",
+                  speed: 4
+                },
+                gifsicle: {
+                  interlaced: false
+                },
+                // https://github.com/tcoopman/image-webpack-loader/issues/111
+                webp: false
+              }
+            }
+          ]
         },
       }
     ]
@@ -58,12 +90,23 @@ const commonConfig = merge([ {
     new webpack.ProvidePlugin({
       'Promise': 'es6-promise',
       'fetch': 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch'
+    }),
+    new HtmlWebpackPlugin({
+        template: "./src/index.html"
     })
   ]
 }]);
 
 const productionConfig = merge([
   parts.extractCSS(),
+  {
+    plugins: [
+      new ImageminPlugin({
+        test: /\.(jpe?g)$/i,
+        plugins: [imageminJpegRecompress({})]
+      })
+    ]
+  }
 ]);
 const developmentConfig = merge([
   {
